c5379f37278a306bc8d6c2b5b03f935432cbbd3c
Conrad <conrad.depeuter@gmail.com>
2017-12-05 19:58:12 -0800

    Add files via upload

--- project/load_retrain_lstm.py
+++ project/load_retrain_lstm.py
@@ -11,7 +11,7 @@ TEST_SIZE= 2084
 iterations = 1000
 maxSeqLength = 50
 numDimensions = 300
-numClasses = 2
+numClasses = 5
 BATCH_SIZE = 2048
 lstmUnits = 16
 
@@ -25,7 +25,7 @@ print("lstmUnits", lstmUnits)
 print("iterations", iterations)
 
 
-with codecs.open("logs/run_"+current_time+".txt",'w') as fp:
+with codecs.open("logs/retrained_"+current_time+".txt",'w') as fp:
     fp.write("time " + current_time + "\n")
     fp.write("BATCH_SIZE " + str(BATCH_SIZE) + "\n")
     fp.write("TEST SIZE " + str(TEST_SIZE) + "\n")
@@ -44,35 +44,23 @@ with codecs.open("logs/run_"+current_time+".txt",'w') as fp:
 # DATA_FILE_START = "w2vreviews"
 
 VECTORS_FILE = "data/vecs/w2v_vectors.npy"
-DATA_FILE_START = "balanced_w2vreviews"
+DATA_FILE_START = "yelp_w2vreviews"
 
 wordVectors = np.load(VECTORS_FILE)
 print("wordVectors shape", wordVectors.shape)
 
 
 
-def getTrainData():
+def getYelpData():
     train_files = [f for f in os.listdir("data/vecs") if f.startswith(DATA_FILE_START) and f.endswith(".npy")]
     
     frames = [np.load("data/vecs/" + f) for f in train_files]
-    labels = [np.load("data/vecs/" + f.replace("reviews", "labels")) for f in train_files]
+    labels = [np.load("data/vecs/" + f.replace("reviews", "ratings")) for f in train_files]
     
     X = np.vstack(frames)
     y = np.vstack(labels)
     
     return X.astype(int),y.astype(int)
-
-train_data, train_labels = getTrainData()
-
-
-MAX_TRAIN_SIZE = 3000000
-train_data = train_data[0:MAX_TRAIN_SIZE]
-train_labels = train_labels[0:MAX_TRAIN_SIZE]
-
-print("train data shape", train_data.shape)
-print("train labels shape", train_labels.shape)
-print("train data max:", train_data.max())
-print("train data balance", train_labels.mean(axis=0))
 
 
 
@@ -85,14 +73,12 @@ def getTrainBatch(size=None):
         #ix = np.array(range(size))
         ix = np.random.randint(train_data.shape[0], size=size)
     
-
-
     return train_data[ix,], train_labels[ix, ]
 
-# def getYelpData():
-#     arr = np.load("data/vecs/_test_yelp_w2vreviews_0.npy")
-#     labels = np.load("data/vecs/_test_yelp_w2vlabels_0.npy")
-#     return arr, labels
+def getYelpTest():
+    arr = np.load("data/vecs/test_yelp_w2vreviews_0.npy")
+    labels = np.load("data/vecs/test_yelp_w2vratings_0.npy")
+    return arr, labels
 
 
 def getTestBatch(size=None):
@@ -117,24 +103,29 @@ def one_hot_label(label):
         return np.array([0,1])
 
 
-# yelp_data, yelp_labels = getYelpData()
-
-# print("yelp data shape", yelp_data.shape)
 
 
+train_data, train_labels = getYelpData()
 
-test_data, test_labels = getTestBatch(size=TEST_SIZE)
+print("train data shape", train_data.shape)
+print("train labels shape", train_labels.shape)
+print("train data max:", train_data.max())
+print("train data balance", train_labels.mean(axis=0))
+
+
+test_data, test_labels = getYelpTest()
 print(len(test_labels), test_labels.shape)
 print(test_data.shape)
 print("test data balance", test_labels.mean(axis=0))
 
 
+tf.reset_default_graph()
+
 labels = tf.placeholder(tf.float32, [None, numClasses])
 input_data = tf.placeholder(tf.int64, [None, maxSeqLength])
-keep_prob = tf.placeholder(tf.float32)
 
-with tf.variable_scope("embeds"):
-    embedding = tf.get_variable(name="word_embedding", shape=wordVectors.shape, initializer=tf.constant_initializer(wordVectors), trainable=False)
+keep_prob = tf.placeholder(tf.float32)
+embedding = tf.get_variable(name="word_embedding", shape=wordVectors.shape, initializer=tf.constant_initializer(wordVectors), trainable=False)
 
 
 #data = tf.Variable(tf.zeros([None, maxSeqLength, numDimensions]),dtype=tf.float32)
@@ -143,19 +134,17 @@ data = tf.nn.embedding_lookup(embedding,input_data)
 
 # lstmCell = tf.contrib.rnn.BasicLSTMCell(lstmUnits,forget_bias=1)
 # outputs,_ =tf.contrib.rnn.static_rnn(lstmCell,input,dtype="float32")
-
 with tf.variable_scope("lstm"):
-
     lstmCell = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
     lstmDropout = tf.contrib.rnn.DropoutWrapper(cell=lstmCell, output_keep_prob=keep_prob)
-    #outputs, states = tf.nn.dynamic_rnn(lstmCell, data, dtype=tf.float32)
+    #outputs, states = tf.nn.dxqynamic_rnn(lstmCell, data, dtype=tf.float32)
 
     value, _ = tf.nn.dynamic_rnn(lstmDropout, data, dtype=tf.float32)
 
 with tf.variable_scope("final_layer"):
-
-    weight = tf.Variable(tf.truncated_normal([lstmUnits, numClasses]), name="last_layer_weights")
-    bias = tf.Variable(tf.constant(0.1, shape=[numClasses]), name="last_layer_biases")
+    # old weights will load, but we dont care about that
+    weight = tf.Variable(tf.truncated_normal([lstmUnits, numClasses]))
+    bias = tf.Variable(tf.constant(0.1, shape=[numClasses]))
     value = tf.transpose(value,   [1, 0, 2])
     last = tf.gather(value, int(value.get_shape()[0]) - 1)
     prediction = (tf.matmul(last, weight) + bias)
@@ -164,41 +153,50 @@ with tf.variable_scope("final_layer"):
 correctPred = tf.equal(tf.argmax(prediction,1), tf.argmax(labels,1))
 accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
 
+all_vars = tf.all_variables()
+print("all variables", [v.name for v in all_vars])
 
+var_names = [v for v in all_vars if v.name.split("/")[0] == 'final_layer']
+print("variables to train")
+print(var_names)
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=labels))
 optimizer = tf.train.AdamOptimizer()
-train_step = optimizer.minimize(loss)
+train_step = optimizer.minimize(loss, var_list=var_names)
+
+
+print("NAMES", var_names)
+
+load_path = "./models/final_lstm20171205-1938.ckpt"
+restore_vars = [v for v in all_vars if v.name.split("/")[0] == "lstm"]
+sess = tf.InteractiveSession()
+saver = tf.train.Saver(var_list=restore_vars)
+# Initialize v1 since the saver will not.
+saver.restore(sess, load_path)
+
 
 tf.summary.scalar('Loss', loss)
 tf.summary.scalar('Accuracy', accuracy)
 merged = tf.summary.merge_all()
-
-
-sess = tf.InteractiveSession()
 logdir = "tensorboard/" + current_time + "/"
 writer = tf.summary.FileWriter(logdir, sess.graph)
 
-# dont save last layer weights
-all_vars = tf.all_variables()
-var_to_restore = [v for v in all_vars if v.name.split('/')[0]=='embed' or v.name.split('/')[0]=='lstm']
-var_names = [v.name for v in var_to_restore]
-saver = tf.train.Saver(var_to_restore)
-
-
-print("variables to restore", var_names)
-
 sess.run(tf.global_variables_initializer())
-
 final_yelp_acc = None
 final_test_acc = None
 
 
 
+
+final_test_acc = accuracy.eval({input_data:test_data, labels: test_labels, keep_prob:1.0})
+print("****************")
+print("test accuracy:  % f" % final_test_acc)
+
 for i in range(iterations):
     #Next Batch of reviews
     nextBatch, nextBatchLabels = getTrainBatch(size=BATCH_SIZE)
-    _, summary, acc, pred, loss_, outputs = sess.run([ train_step, merged, accuracy, prediction, loss, value], {input_data: nextBatch, labels: nextBatchLabels,keep_prob:.75})
+    _, summary, acc, loss_,  = sess.run([ train_step, merged, accuracy, loss], {input_data: nextBatch, labels: nextBatchLabels,keep_prob:.75})
     #Save the network every 10,000 training iterations
+    
     if i % 10 == 0:
         
         writer.add_summary(summary, i)
@@ -211,10 +209,8 @@ for i in range(iterations):
 
     if i % 50 == 0 or i == iterations-1:
         final_test_acc = accuracy.eval({input_data:test_data, labels: test_labels, keep_prob:1.0})
-        # final_yelp_acc = accuracy.eval({input_data:yelp_data, labels: yelp_labels, keep_prob:1.0})
         print("****************")
         print("test accuracy:  % f" % final_test_acc)
-        # print("yelp accuracy:  % f" % final_yelp_acc)
         print("\n\n")
 #         print("mean prediction", np.mean(pred))
 #         print("\n\n")
@@ -228,11 +224,17 @@ with codecs.open("logs/run_final_"+current_time+".txt",'w') as fp:
     fp.write("lstmUnits " + str(lstmUnits) + "\n")
     fp.write("iterations " + str(iterations) + "\n")
     fp.write("test accuracy " + str(final_test_acc) + "\n")
-    # fp.write("yelp accuracy " + str(final_yelp_acc) + "\n")
+    fp.write("yelp accuracy " + str(final_yelp_acc) + "\n")
+
+        
 
 
-save_file_path = "./models/final_lstm"+current_time+".ckpt"
-save_path = saver.save(sess, save_file_path)
-print("saved to %s" % save_file_path)
-writer.close()
+# with codecs.open("logs/run_final_"+current_time+".txt",'w') as fp:
+#     fp.write("time " + current_time + "\n")
+#     fp.write("BATCH_SIZE " + str(BATCH_SIZE) + "\n")
+#     fp.write("TEST SIZE " + str(TEST_SIZE) + "\n")
+#     fp.write("lstmUnits " + str(lstmUnits) + "\n")
+#     fp.write("iterations " + str(iterations) + "\n")
+#     fp.write("test accuracy " + str(final_test_acc) + "\n")
+#     fp.write("yelp accuracy " + str(final_yelp_acc) + "\n")
 
